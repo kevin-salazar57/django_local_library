cf35de6952baa8e03fc2c73b50601aaf25351dd9
Kevin Arnoldo Salazar Abrego <sa16032@ues.edu.sv>
2023-06-13 19:19:54 -0600

    actualizando dependencias

--- render/build.sh
+++ render/build.sh
@@ -2,7 +2,8 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
+python -m pip install --upgrade pip
+pip3 install -r requirements.txt
 poetry install
 
 python manage.py collectstatic --no-input
