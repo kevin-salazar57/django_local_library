6a320207599c078c35a60e4322172fee87c5b59f
Kevin Arnoldo Salazar Abrego <sa16032@ues.edu.sv>
2023-06-13 19:24:46 -0600

    quitando poetry

--- render/build.sh
+++ render/build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 python -m pip install --upgrade pip
 pip3 install -r requirements.txt
-poetry install
+#poetry install
 
 python manage.py collectstatic --no-input
 python manage.py migrate
