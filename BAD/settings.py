c6772c5daeb2cee278f984e5425378742bfa7e6b
Kevin Arnoldo Salazar Abrego <sa16032@ues.edu.sv>
2023-06-13 19:35:33 -0600

    base_dir

--- BAD/settings.py
+++ BAD/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'apps\static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'apps/templates')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
